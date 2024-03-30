616b9cd28b79ce05f9580c2167c0c7d793a5a3f9
Martin Dimitrov <obimartione@gmail.com>
2024-03-30 19:07:20 +0200

    Function for calculating the distance measured by the Ultrasonic sensor

--- diplomna_kod_za_Radar_Arduino_IDE/diplomna_kod_za_Radar_Arduino_IDE.ino
+++ diplomna_kod_za_Radar_Arduino_IDE/diplomna_kod_za_Radar_Arduino_IDE.ino
@@ -109,6 +109,16 @@ void loop() {
   }
 }
 
+int calculateDistance(){ 
+  
+  digitalWrite(trigPin, LOW); 
+  delayMicroseconds(2);
 
+  digitalWrite(trigPin, HIGH); 
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH); 
 
-
+  distance= duration*0.034/2; 
+  return distance;
+}
